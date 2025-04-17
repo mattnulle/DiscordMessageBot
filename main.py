d87695898d850884dd9a8d5d3f85810fc8e54463
Matt Nulle <matt.nulle@gmail.com>
2025-04-17 10:16:32 -0400

    Update to get working on Railway

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
 import discord
 from discord.ext import commands
+
+from flask import Flask
+from threading import Thread
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -9,14 +13,19 @@ intents.message_content = True  # Required to read message content
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Replace with the actual user ID you want to DM
-TARGET_USER_ID = 123456789012345678  
+TARGET_USER_ID = 287387174947520513
 
 # Replace with the channel IDs you want to monitor
-TARGET_CHANNEL_IDS = {111111111111111111, 222222222222222222}
+TARGET_CHANNEL_IDS = {
+    1362245135492059290, 1339298329997082624, 1345189811962642504,
+    1341556002218315909
+}
+
 
 @bot.event
 async def on_ready():
     print(f'Bot is ready. Logged in as {bot.user}')
+
 
 @bot.event
 async def on_message(message):
@@ -34,4 +43,25 @@ async def on_message(message):
 
     await bot.process_commands(message)  # Ensures other commands still work
 
-bot.run("YOUR_BOT_TOKEN")
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return "I'm alive!"
+
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+
+# Call keep_alive() before bot.run()
+keep_alive()
+
+bot.run(os.getenv("DISCORD_BOT_TOKEN"))
